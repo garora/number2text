06496712d945ff6e1098cad177f1df1264ca3803
Gaurav Arora <g_arora@hotmail.com>
2015-04-06 05:41:46 +0530

    corrected parameter

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
  * Converts Number to Text.
  *
  * @param  {Number} num
+ * @param  {String} type
  * @return {String}
  */
 
 var numberToText = require('./lib/numberToText');
 
-module.exports = function (num,language) {
-  return numberToText(num,language);
+module.exports = function (num,type) {
+  return numberToText(num,type);
 };
