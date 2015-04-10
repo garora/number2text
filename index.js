d7ffdb657a6bb10558097d98133e169496398c0d
Gaurav Arora <g_arora@hotmail.com>
2015-04-11 03:19:57 +0530

    added currency support

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
  *
  * @param  {Number} num
  * @param  {String} type
+ * @param  {Boolean} isCurrency
  * @return {String}
  */
 
 var numberToText = require('./lib/numberToText');
 
-module.exports = function (num,type) {
-  return numberToText(num,type);
+module.exports = function (num,type,isCurrency) {
+  return numberToText(num,type,isCurrency);
 };
